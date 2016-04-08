cb67c324e1b05ec08c2c8619b25f67c8b7124018
Nat14 <naryucha14@gmail.com>
2016-04-08 23:17:52 +0000

    added modify devise fro stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  
+  attr_accessor :stripe_card_token
+  def save_with_payment
+    if valid?
+      # get customer id from stripe
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      # user customer id from stripe
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
 end

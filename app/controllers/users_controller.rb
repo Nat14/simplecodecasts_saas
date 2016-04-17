41bbb4dbb6c3c51cd81acf1b652faba4518efc7c
Nat14 <naryucha14@gmail.com>
2016-04-17 19:52:13 +0000

    added community page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
    
-   def show
+    def show
        @user = User.find( params[:id] )
-   end
+    end
+    
+    def index
+    end
     
 end
